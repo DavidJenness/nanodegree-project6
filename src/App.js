2b2c116adc77d6a3016758565d8c6f226aaf8857
David4FSU <David4FSU@hotmail.com>
2018-10-12 07:36:59 -0500

    Basic Routing in place

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import Shelf from './ReactComponents/Shelf'
-import SearchPlus from './ReactComponents/SearchPlus'
 import {Switch,Route} from "react-router-dom"
+import home from "./ReactViews/home"
+import search from "./ReactViews/search"
+
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -19,42 +20,10 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Shelf title="Currently Reading"/>
-                <Shelf title="Want to Read"/>
-                <Shelf title="Read"/>
-               </div>
-            </div>
-            <SearchPlus />
-          </div>
-        )}
+<Switch>
+  <Route exact path = {"/"} component ={home} />
+  <Route exact path = {"/search"} component ={search} />
+</Switch>
       </div>
     )
   }
--- src/ReactViews/home.js
+++ src/ReactViews/home.js
@@ -1,10 +1,24 @@
 import React, {Component} from "react"
-import Shelf from './ReactComponents/Shelf'
-import SearchPlus from './ReactComponents/SearchPlus'
+import Shelf from '../ReactComponents/Shelf'
+import SearchPlus from '../ReactComponents/SearchPlus'
 
 export default class home extends Component {
 
     render() {
-        return(<div />);
+        return(
+            <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <Shelf title="Currently Reading"/>
+                <Shelf title="Want to Read"/>
+                <Shelf title="Read"/>
+               </div>
+            </div>
+            <SearchPlus />
+          </div>
+        );
     }
 }
